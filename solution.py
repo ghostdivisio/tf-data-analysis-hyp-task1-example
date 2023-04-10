b230f526caa4a8e5f660f3df16d49ab4487909ad
ghostdivisio <112753558+ghostdivisio@users.noreply.github.com>
2023-04-10 16:27:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
+import math
+from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1326113898 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.06
+    
+    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1] / 2
+    
+    if (p_value < alpha) and (x_success/x_cnt < y_success/y_cnt):
+      return True
+    else: 
+      return False
